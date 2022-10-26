800a20695013c00cd76290d03afdd3927a9d0e1f
MylesUnlimited <75455763+MylesUnlimited@users.noreply.github.com>
2022-10-26 13:55:16 -0400

    Add files via upload

--- lab1.c
+++ lab1.c
@@ -24,14 +24,17 @@ char* mysteryExplode(const char * str){
 
     char* new;
 
-    int length = strlen(str) + 1;
+    int length = strlen(str);
 
-    new = (char *) malloc(((length)*(length+1))/2);
+    new = (char *) malloc((((length)*(length+1))/2)+1);
 
     for (int p = 0; p < length; p++){
         
         strncat(new, str, p);
     }
+
+    char z ='\0';
+    strcat(new, &z);
 
     return (new);
 }
