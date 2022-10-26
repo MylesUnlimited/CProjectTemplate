e71ef1ba22318a1db8a12b7698c9e3063ccab5cc
MylesUnlimited <75455763+MylesUnlimited@users.noreply.github.com>
2022-10-25 23:27:15 -0400

    Add files via upload

--- lab1.c
+++ lab1.c
@@ -1,5 +1,38 @@
-/*
- * Implement your solution in thi file
- *
- */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+char * readString(char * file){
+
+    char * dest;
+
+    dest = (char *) malloc(100);
+    
+    FILE *fileptr;                          
+    fileptr = fopen(file, "r");   
+
+    fgets(dest, 100, fileptr);
+
+    fclose( fileptr );
+
+    return dest;
+    }
+    
+    
+char* mysteryExplode(char * stuff){
+
+    char* new;
+
+    int length = strlen(stuff) + 1;
+
+    new = (char *) malloc(((length)*(length+1))/2);
+
+    for (int p = 0; p < length; p++){
+        
+        strncat(new, stuff, p);
+    }
+
+    return (new);
+}
 
