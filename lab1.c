40137ae90e23aa53862afb5764eb8d018146e59e
MylesUnlimited <75455763+MylesUnlimited@users.noreply.github.com>
2022-12-03 00:11:32 -0500

    Fixed Newline
    Fixed Newline and tested for 100 character string

--- lab1.c
+++ lab1.c
@@ -13,9 +13,11 @@ char * readString(char * fileName){
     FILE *fileptr;                          
     fileptr = fopen(fileName, "r");   
 
-    fgets(dest, 100, fileptr);
+    fgets(dest, MAX_LINE_LEN, fileptr);
 
     fclose( fileptr );
+
+    dest[strcspn(dest, "\n")] = 0;
 
     return dest;
     }
@@ -23,19 +25,16 @@ char * readString(char * fileName){
     
 char* mysteryExplode(const char * str){
 
-    char* new;
-
     int length = strlen(str);
 
-    new = (char *) malloc((((length)*(length+1))/2)+1);
+    char* new = (char *) malloc((((length)*(length+1))/2)+1);
+
+    memset(new, '\0', (((length)*(length+1))/2)+1);
 
     for (int p = 0; p < length; p++){
         
-        strncat(new, str, p);
+        strncat(new, str, p+1);
     }
-
-    char z ='\0';
-    strcat(new, &z);
 
     return (new);
 }
